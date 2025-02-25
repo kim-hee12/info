75605abe4ccf752dfa1d09648151c071bfe1b9a1
kim-hee12 <a99458440@gmail.com>
2025-02-25 17:44:49 +0900

    Add files via upload

---
+++ java/my peioct/src/ch02/C06정리_문제.java
@@ -0,0 +1,44 @@
+package C02;
+
+public class C06정리_문제 {
+	
+//		정수 타입 		: byte,short,int ,long
+//		char 타입 	: ' ' 
+//		String 타입 	: " "
+//		실수 타입 		: double , float
+//		논리 타입 		: boolean
+//
+//
+//		확인 문제1
+//
+//		정수타입 
+//		1byte : byte
+//		2byte : char / short
+//		4byte : int 
+//		8byte : long 
+//
+//		실수타입
+//		4byte : float
+//		8byte : double
+//
+//		논리타입
+//		1byte : boolean
+//
+//
+//		확인문제2
+//		
+//		
+//		맞는 코드인지 틀린 코드인지 확인 
+//		byte var = 200;							( x	)
+//		char var='AB';							( o )
+//		char var=65;							( o )
+//		long var=50000000000;					( x	)	
+//		float var = 3.14						( x	)
+//		double var = 100.0						( x	)
+//		String var = "나의직업은 "개발자" 입니다.";	( x )
+//		boolean var = 0;						( x )
+//		int v2 = 1e2;							( o	)
+//		float =1e2f;							( x	)
+//	
+
+}
