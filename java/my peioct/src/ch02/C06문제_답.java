5d631de08d67c8b7f80cfe9554309875198e6e55
kim-hee12 <a99458440@gmail.com>
2025-02-26 09:25:53 +0900

    Add files via upload

---
+++ java/my peioct/src/ch02/C06문제_답.java
@@ -0,0 +1,22 @@
+package C02;
+
+public class C06문제_답 {
+
+	public static void main(String[] args) {
+	
+		//	맞는 코드인지 틀린 코드인지 확인
+		
+//		byte var = 200;							( x	)
+//		char var='AB';							( x )
+//		char var=65;							( o )
+//		long var=50000000000;					( x	)	
+//		float var = 3.14						( x	)
+//		double var = 100.0						( o	)
+//		String var = "나의직업은 "개발자" 입니다.";	( x )
+//		boolean var = 0;						( x )
+//		int v2 = 1e2;							( x	)
+//		float =1e2f;							( x	)
+
+	}
+
+}
