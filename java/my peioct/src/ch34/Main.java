e4a9be42088164de575fba86cf5373643a00b844
kim-hee12 <a99458440@gmail.com>
2025-03-21 17:46:52 +0900

    Add files via upload

---
+++ java/my peioct/src/ch34/Main.java
@@ -0,0 +1,34 @@
+package ch34;
+
+@CustomAnnotation(value="hello world",number=5,isOpen=true)
+class Simple{
+	String v1;
+	int v2;
+	
+	Simple(){
+		CustomAnnotation ref = this.getClass().getAnnotation(CustomAnnotation.class);
+		System.out.println("value : "+ ref.value());
+		System.out.println("number : "+ ref.number());
+		System.out.println("isOpen : " + ref.isOpen());
+		this.v1 =ref.value();
+		this.v2 =ref.number();
+	}
+
+	@Override
+	public String toString() {
+		return "Simple [v1=" + v1 + ", v2=" + v2 + "]";
+	}
+	
+	
+}
+
+public class Main {
+
+	public static void main(String[] args) {
+		Simple obj = new Simple();
+		System.out.println(obj);
+
+
+	}
+
+}
